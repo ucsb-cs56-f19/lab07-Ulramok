740c4a727efc975fe9a2bd700f4c2c7614602d4a
Tanay Komarlu <tkomarlu@ucsb.edu>
2019-11-14 19:30:50 -0800

    added code to display json,features returned in json and featureCollection on results.html

--- src/main/java/hello/WebController.java
+++ src/main/java/hello/WebController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import java.util.Map;
 import java.util.HashMap;
+import hello.geojson.FeatureCollection;
 
 import com.nimbusds.oauth2.sdk.client.ClientReadRequest;
 
@@ -57,7 +58,16 @@ public class WebController {
     @GetMapping("/earthquakes/results")
     public String getEarthquakesResults(Model model, OAuth2AuthenticationToken oAuth2AuthenticationToken,
                                        EqSearch eqSearch) {
+        EarthquakeQueryService e = new EarthquakeQueryService();
+
         model.addAttribute("eqSearch", eqSearch);
+
+        String json = e.getJSON(eqSearch.getDistance(), eqSearch.getMinmag());
+        model.addAttribute("json", json);
+
+        FeatureCollection featureCollection = FeatureCollection.fromJSON(json);
+        model.addAttribute("featureCollection",featureCollection);
+
         return "earthquakes/results";
     }
 }
